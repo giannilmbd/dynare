43af30d0c14cb42d8e41a5e4738b1e846aebbced
Frédéric Karamé <frederic.karame@univ-lemans.fr>
2024-09-06 16:04:09 +0200

    Call compute_decision_rules instead of resol.

--- matlab/nonlinear-filters/solve_model_for_online_filter.m
+++ matlab/nonlinear-filters/solve_model_for_online_filter.m
@@ -126,7 +126,8 @@ M_.H = H;
 
 warning('off', 'MATLAB:nearlySingularMatrix')
 [oo_.dr, info, M_.params] = ...
-    resol(0,M_, options_, oo_.dr, oo_.steady_state, oo_.exo_steady_state, oo_.exo_det_steady_state);
+    compute_decision_rules(M_, options_, oo_.dr, oo_.steady_state, oo_.exo_steady_state, oo_.exo_det_steady_state);
+%    resol(0,M_, options_, oo_.dr, oo_.steady_state, oo_.exo_steady_state, oo_.exo_det_steady_state);
 warning('on', 'MATLAB:nearlySingularMatrix')
 
 if info(1)~=0
