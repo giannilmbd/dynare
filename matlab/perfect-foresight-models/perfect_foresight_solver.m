91cf1d59f89da415c2552b7175491b51a867a863
Sébastien Villemot <sebastien@dynare.org>
2025-04-30 12:07:21 +0200

    🐛 Perfect foresight with controlled paths: no longer crash with homotopy_exclude_varexo option and multiple controlled variables

--- matlab/perfect-foresight-models/perfect_foresight_solver.m
+++ matlab/perfect-foresight-models/perfect_foresight_solver.m
@@ -567,7 +567,7 @@ if ~isempty(controlled_paths_by_period)
         % Handle guess values for endogenized exos
         exo_ids = controlled_paths_by_period(p).endogenize_id;
         if ~isempty(options_.simul.homotopy_exclude_varexo)
-            [is_excluded, excluded_exo_ids] = ismember(options_.simul.homotopy_exclude_varexo, M_.exo_names{exo_ids});
+            [is_excluded, excluded_exo_ids] = ismember(options_.simul.homotopy_exclude_varexo, M_.exo_names(exo_ids));
             if any(is_excluded)
                 error('Exogenous %s cannot be in the homotopy_exclude_varexo option and a perfect_foresight_controlled_paths block at the same time', M_.exo_names{excluded_exo_ids(1)})
             end
