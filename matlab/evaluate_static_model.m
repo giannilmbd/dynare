giannilmbd/dynare

commit 0393e3a819bfc54c72f881ace58045d38a7e4b97

evaluate_static_model.m: remove unused output argument

matlab/evaluate_static_model.m
@@ -1,6 +1,5 @@
-function [residuals,check1,jacob] = evaluate_static_model(ys,exo_ss,params,M_,options_)
-
-% function [residuals,check1,jacob] = evaluate_static_model(ys,exo_ss,params,M_,options_)
+function [residuals,jacob] = evaluate_static_model(ys,exo_ss,params,M_,options_)
+% function [residuals,jacob] = evaluate_static_model(ys,exo_ss,params,M_,options_)
 % Evaluates the static model
 %
 % INPUTS
@@ -14,13 +13,12 @@ function [residuals,check1,jacob] = evaluate_static_model(ys,exo_ss,params,M_,op
 % OUTPUTS
 %   residuals                 vector           residuals when ys is not
 %                                              the steady state
-%   check1                    scalar           error flag
 %   jacob                     matrix           Jacobian of static model
 %
 % SPECIAL REQUIREMENTS
 %   none
 
-% Copyright © 2001-2023 Dynare Team
+% Copyright © 2001-2024 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -37,7 +35,6 @@ function [residuals,check1,jacob] = evaluate_static_model(ys,exo_ss,params,M_,op
 % You should have received a copy of the GNU General Public License
 % along with Dynare.  If not, see <https://www.gnu.org/licenses/>.
 
-check1 = 0;
 if options_.bytecode
     if nargout<3
         [residuals]= bytecode('evaluate', 'static', M_, options_, ys, ...

matlab/evaluate_steady_state.m
@@ -83,7 +83,7 @@ if options_.ramsey_policy
         if ~options_.debug
             resids = evaluate_static_model(ys,exo_ss,params,M_,options_);
         else
-            [resids, ~ , jacob]= evaluate_static_model(ys,exo_ss,params,M_,options_);
+            [resids, jacob]= evaluate_static_model(ys,exo_ss,params,M_,options_);
         end
         nan_indices=find(isnan(resids(M_.ramsey_orig_endo_nbr+(1:M_.ramsey_orig_eq_nbr))));
 

matlab/evaluate_steady_state_file.m
@@ -101,12 +101,7 @@ end
 
 if steady_state_checkflag
     % Check whether the steady state obtained from the _steadystate file is a steady state.
-    [residuals, check] = evaluate_static_model(ys, exo_ss, params, M_, options_);
-    if check
-        info(1) = 19;
-        info(2) = check; % to be improved
-        return
-    end
+    residuals = evaluate_static_model(ys, exo_ss, params, M_, options_);
     if max(abs(residuals)) > options_.solve_tolf
         info(1) = 19;
         info(2) = residuals'*residuals;

commit 08510f93161606ec43f9005c09cd8f679e8ec90e

evaluate_static_model.m: fix bug introduced in 0393e3a819bfc54c72f881ace58045d38a7e4b97

matlab/evaluate_static_model.m
@@ -46,7 +46,7 @@ if options_.bytecode
     end      
 else
     [residuals, T_order, T] = feval([M_.fname '.sparse.static_resid'], ys, exo_ss, params);
-    if nargout >= 3
+    if nargout >= 2
         jacob = feval([M_.fname '.sparse.static_g1'], ys, exo_ss, params, M_.static_g1_sparse_rowval, M_.static_g1_sparse_colval, M_.static_g1_sparse_colptr, T_order, T);
     end
 end