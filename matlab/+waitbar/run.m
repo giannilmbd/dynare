giannilmbd/dynare

commit 65f3898b7351ea9b40cc1e5f8d4b51feba3adcc7

waitbar: remove global and persistent variables, simplify logic

matlab/+waitbar/run.m
@@ -1,10 +1,25 @@
-function h = run(prctdone, varargin)
-% h = waitbar(prctdone, varargin)
+function [fig_handle, length_of_old_string]= run(prctdone, wait_handle, running_text, console_mode, move_cursor_backwards, fig_title, whoiam, Parallel)
+% [fig_handle, length_of_old_string]= run(prctdone, wait_handle, running_text, console_mode, move_cursor_backwards, whoiam, Parallel)
 % adaptive waitbar, producing console mode waitbars with
-% octave and when console_mode=1
-
+% GNU Octave and when console_mode=1
+% Inputs
+%  - prctdone               [double]    progress of waitbar
+%  - wait_handle            [handle]    figure handle to waitbar (empty if parallel or console); not required for initialization, i.e. prctdone==0
+%  - running_text           [char]      text displayed above waitbar
+%  - console_mode           [bool]      indicator whether console output is requested
+%  - move_cursor_backwards  [integer]   number of backward cursor movements for console
+%  - fig_title              [string]    option figure title for initialization (prctdone==0)
+%  - whoiam                 [integer]   In concurrent programming a modality to refer to the different threads running in parallel is needed.
+%                                       The integer whoaim is the integer that allows us to distinguish between them. Then it is the 
+%                                       index number of this CPU among all CPUs in the cluster.
+%  - ThisMatlab             [integer]   Allows us to distinguish between the 'main' MATLAB, the slave MATLAB worker, local MATLAB, remote MATLAB,
+%                                       then it is the index number of this slave machine in the cluster.
 %
-% Copyright © 2011-2017 Dynare Team
+% Outputs
+%  - fig_handle             [handle]    figure handle to waitbar (empty if parallel or console)
+%  - length_of_old_string   [integer]   length of last text printed in console
+%
+% Copyright © 2011-2025 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -20,54 +35,45 @@ function h = run(prctdone, varargin)
 %
 % You should have received a copy of the GNU General Public License
 % along with Dynare.  If not, see <https://www.gnu.org/licenses/>.
-global options_
-persistent running_text newString
-persistent whoiam Parallel
-
-if iscell(prctdone)
-    whoiam = prctdone{2};
-    Parallel = prctdone{3};
-    prctdone=prctdone{1};
-end
 
 if prctdone==0
     init=1;
-    if isempty(whoiam)
-        whoiam=0;
-    end
 else
     init=0;
 end
-if nargout
-    h=[];
+
+if nargin< 7
+    fig_title=[];
+end
+if nargin< 7 || isempty(whoiam) 
+    whoiam=0;
 end
 
-if ~whoiam
-    if options_.console_mode
+% initialize outputs
+fig_handle=[];
+length_of_old_string=0;
+
+if ~whoiam %serial execution
+    if console_mode
         if init
             diary off;
-            running_text = varargin{1};
-            newString='';
             return
-        elseif nargin>2
-            running_text =  varargin{2};
-        end
-        s0=repmat('\b',1,length(newString));
+        end        
         newString=sprintf([running_text,' %3.f%% done'], prctdone*100);
-        fprintf([s0,'%s'],newString);
+        move_backwards=repmat('\b',1,move_cursor_backwards);
+        fprintf([move_backwards,'%s'],newString);
+        length_of_old_string=length(newString);
     else
-        if nargout
-            h = waitbar(prctdone,varargin{:});
+        if init
+            fig_handle = waitbar(prctdone,running_text);
+            if ~isempty(fig_title)
+                set(fig_handle,'Name', fig_title);
+            end
         else
-            waitbar(prctdone,varargin{:});
+            fig_handle = waitbar(prctdone,wait_handle,running_text);
         end
     end
-else
-    if init
-        running_text = varargin{1};
-    elseif nargin>2
-        running_text = varargin{2};
-    end
+else %parallel execution
     if Parallel.Local
         waitbarTitle='Local ';
     else

matlab/+identification/run.m
@@ -533,7 +533,8 @@ if iload <=0
     if SampleSize > 1
         % initializations for Monte Carlo Analysis
         fprintf('\nMonte Carlo Testing\n');
-        h = waitbar.run(0,'Monte Carlo identification checks ...');
+        [h, length_of_old_string] = waitbar.run(0,[],'Monte Carlo identification checks ...', options_.console_mode, 0,'Monte Carlo Identification Analysis.');
+
         iteration  = 0; % initialize counter for admissable draws
         run_index  = 0; % initialize counter for admissable draws after saving previous draws to file(s)
         file_index = 0; % initialize counter for files (if options_.MaxNumberOfBytes is reached, we store results in files)
@@ -747,13 +748,13 @@ if iload <=0
                 run_index = 0; % reset index
             end
             if SampleSize > 1 && mod(iteration,3)
-                waitbar.run(iteration/SampleSize, h, ['MC identification checks ', int2str(iteration), '/', int2str(SampleSize)]);
+                [~, length_of_old_string] = waitbar.run(iteration/SampleSize, h, ['MC identification checks ', int2str(iteration), '/', int2str(SampleSize)], options_.console_mode, length_of_old_string);
             end
         end
     end
 
     if SampleSize > 1
-        waitbar.close(h);
+        waitbar.close(h,options_.console_mode);
         normalize_STO_DYNAMIC = std(STO_DYNAMIC,0,2);
         if ~options_MC.no_identification_reducedform
             normalize_STO_REDUCEDFORM = std(STO_REDUCEDFORM,0,2);

matlab/+gsa/map_calibration.m
@@ -13,7 +13,7 @@ function map_calibration(OutputDirectoryName, M_, options_, oo_, estim_params_, 
 % marco.ratto@ec.europa.eu
 
 % Copyright © 2014-2016 European Commission
-% Copyright © 2014-2023 Dynare Team
+% Copyright © 2014-2025 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -105,7 +105,7 @@ if init
     end
 
     irestrictions = 1:Nsam;
-    h = waitbar.run(0,'Please wait...');
+    [h,length_of_old_string]=waitbar.run(0,[],'Please wait...',options_.console_mode,0,'Sensitivity analysis for calibration criteria.');
     for j=1:Nsam
         M_ = set_all_parameters(lpmat(j,:)',estim_params_,M_);
         if nbr_moment_restrictions
@@ -131,10 +131,10 @@ if init
             irestrictions(j)=0;
         end
         if mod(j,3)==0
-            waitbar.run(j/Nsam,h,['MC iteration ',int2str(j),'/',int2str(Nsam)])
+            [~,length_of_old_string]=waitbar.run(j/Nsam,h,['MC iteration ',int2str(j),'/',int2str(Nsam)],options_.console_mode,length_of_old_string);
         end
     end
-    waitbar.close(h);
+    waitbar.close(h,options_.console_mode);
 
     irestrictions=irestrictions(find(irestrictions));
     xmat=lpmat(irestrictions,:);

matlab/+gsa/monte_carlo_moments.m
@@ -13,7 +13,7 @@ function [vdec, cc, ac] = monte_carlo_moments(mm, ss, dr, M_, options_, estim_pa
 % - ac                  [cell]          autocorrelation matrix
 
 
-% Copyright © 2012-2023 Dynare Team
+% Copyright © 2012-2025 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -33,7 +33,8 @@ function [vdec, cc, ac] = monte_carlo_moments(mm, ss, dr, M_, options_, estim_pa
 [~, nc1, nsam] = size(mm);
 nobs=length(options_.varobs);
 disp('monte_carlo_moments: Computing theoretical moments ...')
-h = waitbar.run(0,'Theoretical moments ...');
+[h, length_of_old_string] = waitbar.run(0,[],'monte_carlo_moments: Theoretical moments ...', options_.console_mode, 0, 'Monte Carlo moments.');
+
 vdec = zeros(nobs,M_.exo_nbr,nsam);
 cc = zeros(nobs,nobs,nsam);
 ac = zeros(nobs,nobs*options_.ar,nsam);
@@ -52,9 +53,9 @@ for j=1:nsam
     end
     ac(:,:,j)=dum;
     if mod(j,3)==0
-        waitbar.run(j/nsam,h)
+        [~, length_of_old_string]=waitbar.run(j/nsam,h,'monte_carlo_moments: Theoretical moments ...',options_.console_mode,length_of_old_string);
     end
 end
-waitbar.close(h)
+waitbar.close(h,options_.console_mode)
 skipline()
 disp('... done !')

matlab/+gsa/stability_mapping.m
@@ -44,7 +44,7 @@ function x0 = stability_mapping(OutputDirectoryName,opt_gsa,M_,oo_,options_,baye
 % marco.ratto@ec.europa.eu
 
 % Copyright © 2012-2016 European Commission
-% Copyright © 2012-2023 Dynare Team
+% Copyright © 2012-2025 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -236,7 +236,7 @@ if fload==0 %run new MC
         end
     end
     %
-    h = waitbar.run(0,'Please wait...');
+    [h, length_of_old_string] = waitbar.run(0,[],'Please wait. Stability mapping ...', options_.console_mode, 0, 'Stability mapping.');
     istable=1:Nsam;
     jstab=0;
     iunstable=1:Nsam;
@@ -333,10 +333,10 @@ if fload==0 %run new MC
         ys_=real(dr_.ys);
         yys(:,j) = ys_;
         if mod(j,3)
-            waitbar.run(j/Nsam,h,['MC iteration ',int2str(j),'/',int2str(Nsam)])
+            [~,length_of_old_string]=waitbar.run(j/Nsam,h,['MC iteration ',int2str(j),'/',int2str(Nsam)],options_.console_mode,length_of_old_string);
         end
     end
-    waitbar.close(h);
+    waitbar.close(h,options_.console_mode);
     if prepSA && jstab
         T=T(:,:,1:jstab);
     else
@@ -389,7 +389,8 @@ else %load old run
     end
 
     if prepSA && isempty(strmatch('T',who('-file', filetoload),'exact'))
-        h = waitbar.run(0,'Please wait...');
+        [h, length_of_old_string] = waitbar.run(0,[],'Please wait...', options_.console_mode, 0, 'Stability mapping.');
+
         options_.periods=0;
         options_.nomoments=1;
         options_.irf=0;
@@ -408,10 +409,10 @@ else %load old run
             ys_=real(dr_.ys);
             yys(:,j) = ys_;
             if mod(j,3)
-                waitbar.run(j/ntrans,h,['MC iteration ',int2str(j),'/',int2str(ntrans)])
+                [~, length_of_old_string] = waitbar.run(j/ntrans,h,['MC iteration ',int2str(j),'/',int2str(ntrans)], options_.console_mode, length_of_old_string);
             end
         end
-        waitbar.close(h);
+        waitbar.close(h,options_.console_mode);
         save(filetoload,'T','-append')
     end
 end

matlab/+identification/analysis.m
@@ -474,7 +474,7 @@ if info(1) == 0 %no errors in solution
 
         if advanced
             % here we do not normalize (i.e. we set norm_dMOMENTS=1) as the OLS in identification.bruteforce is very sensitive to norm_dMOMENTS
-            [ide_moments.pars, ide_moments.cosndMOMENTS] = identification.bruteforce(M_.dname,M_.fname,dMOMENTS(ind_dMOMENTS,:), max_dim_cova_group, options_.TeX, options_ident.name_tex, options_ident.tittxt, tol_deriv);
+            [ide_moments.pars, ide_moments.cosndMOMENTS] = identification.bruteforce(M_.dname,M_.fname,dMOMENTS(ind_dMOMENTS,:), max_dim_cova_group, options_.TeX, options_ident.name_tex, options_ident.tittxt, tol_deriv,options_.console_mode);
         end
 
         %here we focus on the unnormalized S and V, which is then used in identification.plot.m and for prior_mc > 1
@@ -532,7 +532,7 @@ if info(1) == 0 %no errors in solution
     if checks_via_subsets
         % identification.checks_via_subsets is only for debugging
         [ide_dynamic, ide_reducedform, ide_moments, ide_spectrum, ide_minimal] = ...
-            identification.checks_via_subsets(ide_dynamic, ide_reducedform, ide_moments, ide_spectrum, ide_minimal, totparam_nbr, modparam_nbr, options_ident, error_indicator);
+            identification.checks_via_subsets(ide_dynamic, ide_reducedform, ide_moments, ide_spectrum, ide_minimal, totparam_nbr, modparam_nbr, options_ident, error_indicator, options_.console_mode);
          if ~error_indicator.identification_minimal
              ide_minimal.minimal_state_space=1;
          else

matlab/+identification/bruteforce.m
@@ -1,17 +1,21 @@
-function [pars, cosnJ] = bruteforce(dname,fname,J, max_dim_cova_group, TeX, name_tex, tittxt, tol_deriv)
-% [pars, cosnJ] = bruteforce(dname,fname,J, max_dim_cova_group, TeX, name_tex, tittxt, tol_deriv)
+function [pars, cosnJ] = bruteforce(dname,fname,J, max_dim_cova_group, TeX, name_tex, tittxt, tol_deriv, console_mode)
+% [pars, cosnJ] = bruteforce(dname,fname,J, max_dim_cova_group, TeX, name_tex, tittxt, tol_deriv, console_mode)
 % -------------------------------------------------------------------------
 % given the Jacobian matrix J of moment derivatives w.r.t. parameters
 % computes, for  each column of J, the groups of columns from 1 to n that
 % can replicate at best the derivatives of that column
 % =========================================================================
 % INPUTS
-%  J                  [double] (normalized) Jacobian matrix of moment derivatives
-%  max_dim_cova_group [scalar] maximum size of covariance groups tested
-%  TeX                [scalar] Indicator whether TeX-output is requested
-%  pnames_TeX         [char] list of tex names
+%  dname              [char]    directory name for saving
+%  fname              [char]    file name for saving
+%  J                  [double]  (normalized) Jacobian matrix of moment derivatives
+%  max_dim_cova_group [scalar]  maximum size of covariance groups tested
+%  TeX                [scalar]  Indicator whether TeX-output is requested
+%  name_tex           [char]    list of parameter tex names
 %  tittxt             [string]  string indicating the title text for
 %                               graphs and figures
+%  tol_deriv          [double]  tolerance for singularity
+%  console_mode       [boolean] indicator for whether waitbar is in console mode
 % -------------------------------------------------------------------------
 % OUTPUTS
 %  pars  : cell array with group of params for each column of J for 1 to n
@@ -52,6 +56,9 @@ end
 if nargin < 8
     tol_deriv = 1.e-8;
 end
+if nargin < 9
+    console_mode=false;
+end
 
 tittxt1=regexprep(tittxt, ' ', '_');
 tittxt1=strrep(tittxt1, '.', '');
@@ -59,7 +66,8 @@ tittxt1=strrep(tittxt1, '.', '');
 cosnJ = zeros(totparam_nbr,max_dim_cova_group); %initialize
 pars{totparam_nbr,max_dim_cova_group}=[];       %initialize
 for ll = 1:max_dim_cova_group
-    h = waitbar.run(0,['Brute force collinearity for ' int2str(ll) ' parameters.']);
+    wait_string=['Brute force collinearity for ' int2str(ll) ' parameters.'];
+    [h,length_of_old_string]=waitbar.run(0,[],wait_string,console_mode,0,'Brute force collinearity test.');
     for ii = 1:totparam_nbr
         tmp = find([1:totparam_nbr]~=ii);
         tmp2  = nchoosek(tmp,ll); %find all possible combinations, ind16 could speed this up
@@ -81,9 +89,9 @@ for ll = 1:max_dim_cova_group
         else
             pars{ii,ll} = NaN(1,ll);
         end
-        waitbar.run(ii/totparam_nbr,h)
+        [~,length_of_old_string]=waitbar.run(ii/totparam_nbr,h,wait_string,console_mode,length_of_old_string);
     end
-    waitbar.close(h);
+    waitbar.close(h,console_mode);
     if TeX
         filename = [OutputDirectoryName '/' fname '_collin_patterns_',tittxt1,'_' int2str(ll) '.tex'];
         fidTeX = fopen(filename,'w');

matlab/+identification/checks_via_subsets.m
@@ -1,5 +1,5 @@
-function [ide_dynamic, ide_reducedform, ide_moments, ide_spectrum, ide_minimal] = checks_via_subsets(ide_dynamic, ide_reducedform, ide_moments, ide_spectrum, ide_minimal, totparam_nbr, modparam_nbr, options_ident,error_indicator)
-%[ide_dynamic, ide_reducedform, ide_moments, ide_spectrum, ide_minimal] = checks_via_subsets(ide_dynamic, ide_reducedform, ide_moments, ide_spectrum, ide_minimal, totparam_nbr, modparam_nbr, options_ident,error_indicator)
+function [ide_dynamic, ide_reducedform, ide_moments, ide_spectrum, ide_minimal] = checks_via_subsets(ide_dynamic, ide_reducedform, ide_moments, ide_spectrum, ide_minimal, totparam_nbr, modparam_nbr, options_ident,error_indicator, console_mode)
+%[ide_dynamic, ide_reducedform, ide_moments, ide_spectrum, ide_minimal] = checks_via_subsets(ide_dynamic, ide_reducedform, ide_moments, ide_spectrum, ide_minimal, totparam_nbr, modparam_nbr, options_ident,error_indicator, console_mode)
 % -------------------------------------------------------------------------
 % Finds problematic sets of parameters via checking the necessary rank condition
 % of the Jacobians for all possible combinations of parameters. The rank is
@@ -38,8 +38,9 @@ function [ide_dynamic, ide_reducedform, ide_moments, ide_spectrum, ide_minimal] 
 %                       (Komunjer and Ng, 2011). If either options_ident.no_identification_minimal or
 %                       error_indicator.identification_minimal are 1 then the search for problematic parameter sets will be skipped
 %   totparam_nbr:       [integer] number of estimated stderr, corr and model parameters
-%   numzerotolrank:     [double] tolerance level for rank compuations
+%   numzerotolrank:     [double] tolerance level for rank computations
 %   error_indicator     [structure] indicators whether objects could be computed
+%   console_mode        [bool]      indicator whether console mode was requested
 % -------------------------------------------------------------------------
 % OUTPUTS
 %   ide_reducedform, ide_moments, ide_spectrum, ide_minimal are augmented by the
@@ -52,7 +53,7 @@ function [ide_dynamic, ide_reducedform, ide_moments, ide_spectrum, ide_minimal] 
 % This function is called by
 %   * identification.analysis.m
 % =========================================================================
-% Copyright © 2019-2021 Dynare Team
+% Copyright © 2019-2025 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -322,7 +323,9 @@ end
 indtotparam = unique([indparam_dDYNAMIC indparam_dREDUCEDFORM indparam_dMOMENTS indparam_dSPECTRUM indparam_dMINIMAL]);
 
 for j=2:min(length(indtotparam),max_dim_subsets_groups) % Check j-element subsets
-    h = waitbar.run(0,['Brute force collinearity for ' int2str(j) ' parameters.']);
+    waitbar_string=['Brute force collinearity for ' int2str(j) ' parameters.'];
+    [h, length_of_old_string] = waitbar.run(0, [], waitbar_string, console_mode, 0,'Brute force collinearity test.');
+
     %Step1: get all possible unique subsets of j elements
     if ~no_identification_dynamic ...
             || (~no_identification_reducedform && ~error_indicator.identification_reducedform)...
@@ -424,7 +427,7 @@ for j=2:min(length(indtotparam),max_dim_subsets_groups) % Check j-element subset
                 end
             end
         end
-        waitbar.run(k/maxk,h)
+        [~,length_of_old_string]=waitbar.run(k/maxk,h, waitbar_string, console_mode, length_of_old_string);
     end
 
     %Step 4: Compare rank conditions for all possible subsets. If rank condition is violated, then the corresponding numbers of the parameters are stored
@@ -473,7 +476,7 @@ for j=2:min(length(indtotparam),max_dim_subsets_groups) % Check j-element subset
 %             indtotparam(ismember(indtotparam,idx2(:,2))) = [];
 %         end
 %     end
-    waitbar.close(h);
+    waitbar.close(h,console_mode);
 end
 
 %% Save output variables

commit d2b8098060c40cfae5640e3d257b82a16c903bcd

waitbar.run: correct wrong input count introduced in 65f3898b

matlab/+waitbar/run.m
@@ -42,7 +42,7 @@ else
     init=0;
 end
 
-if nargin< 7
+if nargin< 6
     fig_title=[];
 end
 if nargin< 7 || isempty(whoiam) 