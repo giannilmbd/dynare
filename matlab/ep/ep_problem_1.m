90692ad4e84a2e9efe74f3f4b6a0d2b6af06478d
Sébastien Villemot <sebastien@dynare.org>
2025-09-18 16:16:37 +0200

    Add missing copyright notice

--- matlab/ep/ep_problem_1.m
+++ matlab/ep/ep_problem_1.m
@@ -16,6 +16,23 @@ function [res, A, info] = ep_problem_1(y, x, pfm)
 % REMARKS:
 % [1] The structure pfm holds the given initial condition for the states (pfm.y0) and the terminal condition
 
+% Copyright © 2025 Dynare Team
+%
+% This file is part of Dynare.
+%
+% Dynare is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% Dynare is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Dynare.  If not, see <https://www.gnu.org/licenses/>.
+
 info = false;
 A = [];
 
