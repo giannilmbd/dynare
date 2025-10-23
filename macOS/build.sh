165f4e15d6e3e78f935302658bdd106744fdb0c3
Sébastien Villemot <sebastien@dynare.org>
2025-10-23 17:49:24 +0200

    Build system: use -static-libquadmath flag from GCC 13
    By the way, refactor the way we handle static linking of libgfortran and
    libquadmath in MEX files which have both Fortran and another language (in which
    case the linker is not gfortran).

--- macOS/build.sh
+++ macOS/build.sh
@@ -46,18 +46,7 @@ path_prepend PATH /Library/TeX/texbin
 # Set dependency directory
 LIB64="$ROOTDIR"/macOS/deps/"$PKG_ARCH"/lib64
 
-## Hack for statically linking libquadmath, similar to the one used in
-## deps/Makefile for several libraries (there is no -static-libquadmath flag,
-## see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=46539).
-##
-## NB: The hack done for Windows does not work for two reasons:
-## - the macOS linker is different from GNU ld and does not have the equivalent of -Bstatic/-Bdynamic
-## - libgfortran.spec does not include --as-needed on macOS, hence it will link the library anyways
-## Also, it does not seem possible to override libgfortran.spec with the --specs option.
 GCC_VERSION=$(sed -En "/^c[[:space:]]*=/s/c[[:space:]]*=[[:space:]]*'.*gcc-([0-9]+)'/\1/p" "$ROOTDIR"/macOS/homebrew-native-"$PKG_ARCH".ini)
-
-QUADMATH_DIR=$(mktemp -d)
-ln -s "$BREWDIR"/opt/gcc/lib/gcc/"$GCC_VERSION"/libquadmath.a "$QUADMATH_DIR"
 
 ##
 ## Compile Dynare
@@ -66,7 +55,7 @@ cd "$ROOTDIR"
 
 # NB: the addition of -Wl,-ld_classic is a workaround for https://github.com/mesonbuild/meson/issues/12282 (see also the native file)
 common_meson_opts=(-Dbuild_for=matlab --buildtype=release --prefer-static -Dfortran_args="[ '-B', '$LIB64/Slicot/' ]" \
-                   -Dc_link_args="[ '-Wl,-ld_classic', '-L$QUADMATH_DIR' ]" -Dcpp_link_args="[ '-Wl,-ld_classic', '-L$QUADMATH_DIR' ]" -Dfortran_link_args="[ '-Wl,-ld_classic', '-L$QUADMATH_DIR' ]" \
+                   -Dc_link_args="[ '-Wl,-ld_classic' ]" -Dcpp_link_args="[ '-Wl,-ld_classic' ]" -Dfortran_link_args="[ '-Wl,-ld_classic' ]" \
                    --native-file macOS/homebrew-native-$PKG_ARCH.ini)
 
 # Build for MATLAB ⩾ R2020a (x86_64) and MATLAB ⩾ R2023b (arm64)
