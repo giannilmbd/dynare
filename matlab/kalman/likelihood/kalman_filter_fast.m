b441f47b08b526a3a9c4ccf814b59e7f33f45a34
Johannes Pfeifer <jpfeifer@gmx.de>
2025-11-26 00:23:40 +0100

    fast_kalman_filter: clean up codes
    - remove inoperational code paths related to steady state Kalman filter and analytical derivation
    - remove unused input arguments
    - remove unused unit rests

--- matlab/kalman/likelihood/kalman_filter_fast.m
+++ matlab/kalman/likelihood/kalman_filter_fast.m
@@ -1,80 +1,33 @@
-function [LIK, LIKK, a, P] = kalman_filter_fast(Y,start,last,a,P,kalman_tol,riccati_tol,presample,T,Q,R,H,Z,mm,pp,rr,Zflag,diffuse_periods,analytic_derivation,DT,DYss,DOm,DH,DP,D2T,D2Yss,D2Om,D2H,D2P)
-% [LIK, LIKK, a, P] =
-% kalman_filter_fast(Y,start,last,a,P,kalman_tol,riccati_tol,presample,T,Q,R,H,Z,mm,pp,rr,Zflag,diffuse_periods,analytic_derivation,DT,DYss,DOm,DH,DP,D2T,D2Yss,D2Om,D2H,D2P)
+function [LIK, LIKK, a, P] = kalman_filter_fast(Y,start,last,a,P,kalman_tol,presample,T,H,Z,pp,Zflag,diffuse_periods)
+% [LIK, LIKK, a, P] = kalman_filter_fast(Y,start,last,a,P,kalman_tol,presample,T,H,Z,pp,Zflag,diffuse_periods)
 % computes the likelihood of a stationnary state space model using Ed
 % Herbst fast implementation of the Kalman filter.
+%
+% Inputs:
+% - Y           [double]        pp*T matrix of data
+% - scalar      [integer]       first period.
+% - last        [integer]       last period, last- first has to be smaller than T
+% - a           [double]        mm*1 vector of predicted initial state variables E_{0}(alpha_1)
+% - P           [double]        mm*mm initial covariance matrix of the state vector
+% - kalman_tol  [double]        tolerance parameter (rcond, invertibility of the covariance matrix of the prediction errors).
+% - presample   [integer]       number of initial iterations to be discarded when evaluating the likelihood
+% - T           [double]        mm*mm state transition matrix
+% - H           [double]        pp*pp covariance matrix of the measurement errors (if no measurement errors set H as a zero scalar).
+% - Z           [double]        pp*mm matrix matrix relating states to  observed variables or vector of indices (depending on the value of Zflag).
+% - pp          [integer]       number of observed variables
+% - Zflag       [boolean]       equal to 0 if Z is a vector of indices targeting the obseved variables in the state vector,
+%                               equal to 1 if Z is a pp*mm} matrix
+% - diffuse_periods [integer]       number of diffuse filter periods in the initialization step
+%
+% Outputs
+% - LIK         [double]        scalar value of (minus) the likelihood
+% - likk        [double]        column vector of density of each observation
+% - a           [double]        mm*1 mean of the state vector at the end of the (sub)sample (E_{T}(alpha_{T+1})).
+% - P           [double]        mm*mm covariance of the state vector at the end of the (sub)sample
+%
+% This function is called by: dsge_likelihood
 
-%@info:
-%! @deftypefn {Function File} {[@var{LIK},@var{likk},@var{a},@var{P} ] =} kalman_filter_fast(@var{Y}, @var{start}, @var{last}, @var{a}, @var{P}, @var{kalman_tol}, @var{riccati_tol},@var{presample},@var{T},@var{Q},@var{R},@var{H},@var{Z},@var{mm},@var{pp},@var{rr},@var{Zflag},@var{diffuse_periods})
-%! @anchor{kalman_filter}
-%! @sp 1
-%! Computes the likelihood of a stationary state space model, given initial condition for the states (mean and variance).
-%! @sp 2
-%! @strong{Inputs}
-%! @sp 1
-%! @table @ @var
-%! @item Y
-%! Matrix (@var{pp}*T) of doubles, data.
-%! @item start
-%! Integer scalar, first period.
-%! @item last
-%! Integer scalar, last period (@var{last}-@var{first} has to be inferior to T).
-%! @item a
-%! Vector (@var{mm}*1) of doubles, levels of the predicted initial state variables (E_{0}(alpha_1)).
-%! @item P
-%! Matrix (@var{mm}*@var{mm}) of doubles, initial covariance matrix of the state vector.
-%! @item kalman_tol
-%! Double scalar, tolerance parameter (rcond, inversibility of the covariance matrix of the prediction errors).
-%! @item riccati_tol
-%! Double scalar, tolerance parameter (iteration over the Riccati equation).
-%! @item presample
-%! Integer scalar, presampling if strictly positive (number of initial iterations to be discarded when evaluating the likelihood).
-%! @item T
-%! Matrix (@var{mm}*@var{mm}) of doubles, transition matrix of the state equation.
-%! @item Q
-%! Matrix (@var{rr}*@var{rr}) of doubles, covariance matrix of the structural innovations (noise in the state equation).
-%! @item R
-%! Matrix (@var{mm}*@var{rr}) of doubles, second matrix of the state equation relating the structural innovations to the state variables.
-%! @item H
-%! Matrix (@var{pp}*@var{pp}) of doubles, covariance matrix of the measurement errors (if no measurement errors set H as a zero scalar).
-%! @item Z
-%! Matrix (@var{pp}*@var{mm}) of doubles or vector of integers, matrix relating the states to the observed variables or vector of indices (depending on the value of @var{Zflag}).
-%! @item mm
-%! Integer scalar, number of state variables.
-%! @item pp
-%! Integer scalar, number of observed variables.
-%! @item rr
-%! Integer scalar, number of structural innovations.
-%! @item Zflag
-%! Integer scalar, equal to 0 if Z is a vector of indices targeting the obseved variables in the state vector, equal to 1 if Z is a @var{pp}*@var{mm} matrix.
-%! @item diffuse_periods
-%! Integer scalar, number of diffuse filter periods in the initialization step.
-%! @end table
-%! @sp 2
-%! @strong{Outputs}
-%! @sp 1
-%! @table @ @var
-%! @item LIK
-%! Double scalar, value of (minus) the likelihood.
-%! @item likk
-%! Column vector of doubles, values of the density of each observation.
-%! @item a
-%! Vector (@var{mm}*1) of doubles, mean of the state vector at the end of the (sub)sample (E_{T}(alpha_{T+1})).
-%! @item P
-%! Matrix (@var{mm}*@var{mm}) of doubles, covariance of the state vector at the end of the (sub)sample.
-%! @end table
-%! @sp 2
-%! @strong{This function is called by:}
-%! @sp 1
-%! @ref{DsgeLikelihood}
-%! @sp 2
-%! @strong{This function calls:}
-%! @sp 1
-%! @ref{kalman_filter_ss}
-%! @end deftypefn
-%@eod:
-
-% Copyright © 2004-2017 Dynare Team
+% Copyright © 2004-2025 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -91,18 +44,13 @@ function [LIK, LIKK, a, P] = kalman_filter_fast(Y,start,last,a,P,kalman_tol,ricc
 % You should have received a copy of the GNU General Public License
 % along with Dynare.  If not, see <https://www.gnu.org/licenses/>.
 
-
 % Set defaults.
-if nargin<17
+if nargin<12
     Zflag = 0;
 end
 
-if nargin<18
+if nargin<13
     diffuse_periods = 0;
-end
-
-if nargin<19
-    analytic_derivation = 0;
 end
 
 if isempty(Zflag)
@@ -117,18 +65,10 @@ end
 smpl = last-start+1;
 
 % Initialize some variables.
-dF   = 1;
-QQ   = R*Q*transpose(R);   % Variance of R times the vector of structural innovations.
 t    = start;              % Initialization of the time index.
 likk = zeros(smpl,1);      % Initialization of the vector gathering the densities.
 LIK  = Inf;                % Default value of the log likelihood.
-oldK = Inf;
-notsteady   = 1;
 F_singular  = 1;
-asy_hess=0;
-
-DLIK=[];
-Hess=[];
 LIKK=[];
 
 if Zflag
@@ -143,7 +83,7 @@ iF = inv(F);
 Kg = K*iF;
 M = -iF;
 
-while notsteady && t<=last
+while t<=last
     s = t-start+1;
     if Zflag
         v  = Y(:,t)-Z*a;
@@ -178,8 +118,6 @@ while notsteady && t<=last
             Kg = K*iF;
             W = T*W - Kg*W(Z,:);
         end
-        %        notsteady = max(abs(K(:)-oldK))>riccati_tol;
-        oldK = K(:);
     end
     t = t+1;
 end
@@ -190,52 +128,10 @@ end
 
 % Add observation's densities constants and divide by two.
 likk(1:s) = .5*(likk(1:s) + pp*log(2*pi));
-if analytic_derivation
-    DLIK = DLIK/2;
-    dlikk = dlikk/2;
-    if analytic_derivation==2 || asy_hess
-        if asy_hess==0
-            Hess = Hess + tril(Hess,-1)';
-        end
-        Hess = -Hess/2;
-    end
-end
-
-% Call steady state Kalman filter if needed.
-if t <= last
-    if analytic_derivation
-        if analytic_derivation==2
-            [tmp, tmp2] = kalman_filter_ss(Y,t,last,a,T,K,iF,log(dF),Z,pp,Zflag, ...
-                                           analytic_derivation,Da,DT,DYss,D2a,D2T,D2Yss);
-        else
-            [tmp, tmp2] = kalman_filter_ss(Y,t,last,a,T,K,iF,log(dF),Z,pp,Zflag, ...
-                                           analytic_derivation,Da,DT,DYss,asy_hess);
-        end
-        likk(s+1:end)=tmp2{1};
-        dlikk(s+1:end,:)=tmp2{2};
-        DLIK = DLIK + tmp{2};
-        if analytic_derivation==2 || asy_hess
-            Hess = Hess + tmp{3};
-        end
-    else
-        [~, likk(s+1:end)] = kalman_filter_ss(Y, t, last, a, T, K, iF, log(dF), Z, pp, Zflag);
-    end
-end
 
 % Compute minus the log-likelihood.
 if presample>diffuse_periods
     LIK = sum(likk(1+(presample-diffuse_periods):end));
 else
     LIK = sum(likk);
-end
-
-if analytic_derivation
-    if analytic_derivation==2 || asy_hess
-        LIK={LIK, DLIK, Hess};
-    else
-        LIK={LIK, DLIK};
-    end
-    LIKK={likk, dlikk};
-else
-    LIKK=likk;
 end
