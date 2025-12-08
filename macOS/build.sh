giannilmbd/dynare

commit 6b94cb675a39f7d27cece5ba1f82c733076fbbf3

Windows package: clean up meson build directories upon exit

windows/build.sh
@@ -32,13 +32,14 @@ ROOT_DIRECTORY=$(dirname "$(readlink -f "$0")")
 [[ -d /tmp/windeps ]] && { echo "Please remove the /tmp/windeps directory" 2>&1; exit 1; }
 [[ -d "$ROOT_DIRECTORY"/../build-win-matlab ]] && { echo "Please remove the build-win-matlab directory" 2>&1; exit 1; }
 [[ -d "$ROOT_DIRECTORY"/../build-win-octave ]] && { echo "Please remove the build-win-octave directory" 2>&1; exit 1; }
+[[ -d "$ROOT_DIRECTORY"/../build-doc ]] && { echo "Please remove the build-doc directory" 2>&1; exit 1; }
 
 # Create temporary folder and make sure it is deleted upon exit (along with /tmp/windeps)
 TMP_DIRECTORY=$(mktemp -d)
 cleanup()
 {
     [[ -z $TMP_DIRECTORY ]] || rm -rf -- "$TMP_DIRECTORY"
-    rm -rf /tmp/windeps
+    rm -rf /tmp/windeps "$ROOT_DIRECTORY"/../build-win-matlab "$ROOT_DIRECTORY"/../build-win-octave "$ROOT_DIRECTORY"/../build-doc
 }
 trap cleanup EXIT
 

commit e9b97778cf30ede25267fe8576393bf16ab574eb

Windows and macOS package: clean build-doc directory only when not in the CI
Bug introduced in commits 6b94cb675a39f7d27cece5ba1f82c733076fbbf3 and
3182e2d4e21584a4d50238fb83c9b50549da2c94.

macOS/build.sh
@@ -25,11 +25,12 @@ ROOTDIR=$(dirname "$(readlink -f "$0")")/..
 
 # Check that build directories do not already exist
 [[ -d "$ROOTDIR"/build-macOS-matlab ]] && { echo "Please remove the build-macOS-matlab directory" 2>&1; exit 1; }
-[[ -d "$ROOTDIR"/build-doc ]] && { echo "Please remove the build-doc directory" 2>&1; exit 1; }
+[[ -z $CI ]] && [[ -d "$ROOTDIR"/build-doc ]] && { echo "Please remove the build-doc directory" 2>&1; exit 1; }
 
 cleanup()
 {
-    rm -rf "$ROOTDIR"/build-macOS-matlab "$ROOTDIR"/build-doc
+    rm -rf "$ROOTDIR"/build-macOS-matlab
+    [[ -n $CI ]] || rm -rf "$ROOTDIR"/build-doc
 }
 trap cleanup EXIT
 

windows/build.sh
@@ -32,14 +32,15 @@ ROOT_DIRECTORY=$(dirname "$(readlink -f "$0")")
 [[ -d /tmp/windeps ]] && { echo "Please remove the /tmp/windeps directory" 2>&1; exit 1; }
 [[ -d "$ROOT_DIRECTORY"/../build-win-matlab ]] && { echo "Please remove the build-win-matlab directory" 2>&1; exit 1; }
 [[ -d "$ROOT_DIRECTORY"/../build-win-octave ]] && { echo "Please remove the build-win-octave directory" 2>&1; exit 1; }
-[[ -d "$ROOT_DIRECTORY"/../build-doc ]] && { echo "Please remove the build-doc directory" 2>&1; exit 1; }
+[[ -z $CI ]] && [[ -d "$ROOT_DIRECTORY"/../build-doc ]] && { echo "Please remove the build-doc directory" 2>&1; exit 1; }
 
 # Create temporary folder and make sure it is deleted upon exit (along with /tmp/windeps)
 TMP_DIRECTORY=$(mktemp -d)
 cleanup()
 {
     [[ -z $TMP_DIRECTORY ]] || rm -rf -- "$TMP_DIRECTORY"
-    rm -rf /tmp/windeps "$ROOT_DIRECTORY"/../build-win-matlab "$ROOT_DIRECTORY"/../build-win-octave "$ROOT_DIRECTORY"/../build-doc
+    rm -rf /tmp/windeps "$ROOT_DIRECTORY"/../build-win-matlab "$ROOT_DIRECTORY"/../build-win-octave
+    [[ -n $CI ]] || rm -rf "$ROOT_DIRECTORY"/../build-doc
 }
 trap cleanup EXIT
 