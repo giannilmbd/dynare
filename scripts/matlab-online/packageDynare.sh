6874e4f36512ebfb7b01a9cd0a94560402808ee5
Sébastien Villemot <sebastien@dynare.org>
2025-12-12 17:46:09 +0100

    MATLAB Online package: cosmetics
    Use variable names more consistent with what we use for Windows and macOS
    packages.

--- scripts/matlab-online/packageDynare.sh
+++ scripts/matlab-online/packageDynare.sh
@@ -6,10 +6,10 @@ set -exo pipefail
 # The required Ubuntu version can be obtained by running “!lsb_release -a” in
 # MATLAB Online.
 
-X13ASVER=1-1-b62
-MATLABVER=R2025b
+X13AS_VERSION=1-1-b62
+MATLAB_VERSION=R2025b
 
-MATLABPATH=/opt/MATLAB/${MATLABVER}
+MATLAB_PATH=/opt/MATLAB/${MATLAB_VERSION}
 # TODO: change size and put white background for better rendering in MATLAB Add-Ons browser
 DYNARE_PNG_LOGO=../../preprocessor/doc/logos/dlogo.png
 
@@ -23,28 +23,28 @@ trap cleanup EXIT
 
 # Get X13 binary from the Census Bureau website if not cached, and unpack it into the temporary workspace
 # The binary from Ubuntu has some shared library dependencies, so it is safer to use a static binary
-X13ASTARBALL=x13as_ascii-v${X13ASVER}.tar.gz
-if [[ ! -f ${X13ASTARBALL} ]]; then
-    wget --no-verbose --retry-connrefused --retry-on-host-error https://www2.census.gov/software/x-13arima-seats/x13as/unix-linux/program-archives/${X13ASTARBALL}
+X13AS_TARBALL=x13as_ascii-v${X13AS_VERSION}.tar.gz
+if [[ ! -f ${X13AS_TARBALL} ]]; then
+    wget --no-verbose --retry-connrefused --retry-on-host-error https://www2.census.gov/software/x-13arima-seats/x13as/unix-linux/program-archives/${X13AS_TARBALL}
 fi
-tar -xf ${X13ASTARBALL} -C "${tmpdir}"
+tar -xf ${X13AS_TARBALL} -C "${tmpdir}"
 
 pushd ../..
-meson setup -Dbuild_for=matlab -Dmatlab_path="$MATLABPATH" --buildtype=release -Db_lto=true --prefer-static "$tmpdir"/build-matlab-online
+meson setup -Dbuild_for=matlab -Dmatlab_path="$MATLAB_PATH" --buildtype=release -Db_lto=true --prefer-static "$tmpdir"/build-matlab-online
 
 cd "$tmpdir"/build-matlab-online
 meson compile -v
 meson install --destdir "$tmpdir"
-DYNAREVER=$(meson introspect --projectinfo | jq -r '.version')
+DYNARE_VERSION=$(meson introspect --projectinfo | jq -r '.version')
 
 # Sanitize the version number so that is corresponds to MATLAB toolbox
 # requirements: the version must be a scalar string or character vector of the
 # form Major.Minor.Bug.Build, where Bug and Build are optional.
 # Hence remove any character which is not a number or a dot, and ensure that we
 # have at least a minor version number.
-DYNAREVER_SANITIZED=${DYNAREVER//[^0-9.]/}
-if [[ ${DYNAREVER_SANITIZED} != *.* ]]; then
-    DYNAREVER_SANITIZED=${DYNAREVER_SANITIZED}.0
+DYNARE_VERSION_SANITIZED=${DYNARE_VERSION//[^0-9.]/}
+if [[ ${DYNARE_VERSION_SANITIZED} != *.* ]]; then
+    DYNARE_VERSION_SANITIZED=${DYNARE_VERSION_SANITIZED}.0
 fi
 
 cd ..
@@ -58,4 +58,4 @@ cp -p x13as/x13as_ascii dynare/matlab/dseries/externals/x13/linux/64/x13as
 
 # make toolbox
 popd
-"$MATLABPATH/bin/matlab" -batch "packageDynare('$tmpdir/dynare', '$DYNAREVER', '$DYNAREVER_SANITIZED', '$DYNARE_PNG_LOGO')"
+"$MATLAB_PATH/bin/matlab" -batch "packageDynare('$tmpdir/dynare', '$DYNARE_VERSION', '$DYNARE_VERSION_SANITIZED', '$DYNARE_PNG_LOGO')"
