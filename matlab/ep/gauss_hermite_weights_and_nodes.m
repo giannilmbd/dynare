86acaeaf8715decf8710fd547bb3783427dd90b6
Stéphane Adjemian (Scylla) <stepan@adjemian.eu>
2026-01-05 11:04:04 +0100

    Activate unit tests.

--- matlab/ep/gauss_hermite_weights_and_nodes.m
+++ matlab/ep/gauss_hermite_weights_and_nodes.m
@@ -35,66 +35,68 @@ JacobiEigenVector = transpose(JacobiEigenVector(idx));
 weights = JacobiEigenVector.^2;
 nodes = sqrt(2)*nodes;
 
+return % --*-- Unit tests --*--
+
 %@test:1
-%$ n = 5;
-%$ [nodes,weights] = gauss_hermite_weights_and_nodes(n);
-%$
-%$ sum_of_weights = sum(weights);
-%$
-%$ % Expected nodes (taken from Judd (1998, table 7.4).
-%$ enodes = [-2.020182870; -0.9585724646; 0; 0.9585724646;   2.020182870];
-%$
-%$ % Check the results.
-%$ t(1) = dassert(1.0,sum_of_weights,1e-12);
-%$ t(2) = dassert(enodes,nodes/sqrt(2),1e-8);
-%$ T = all(t);
+n = 5;
+[nodes,weights] = gauss_hermite_weights_and_nodes(n);
+
+sum_of_weights = sum(weights);
+
+% Expected nodes (taken from Judd (1998, table 7.4).
+enodes = [-2.020182870; -0.9585724646; 0; 0.9585724646;   2.020182870];
+
+% Check the results.
+t(1) = dassert(1.0,sum_of_weights,1e-12);
+t(2) = dassert(enodes,nodes/sqrt(2),1e-8);
+T = all(t);
 %@eof:1
 
 %@test:2
-%$ n = 9;
-%$ [nodes,weights] = gauss_hermite_weights_and_nodes(n);
-%$
-%$ sum_of_weights = sum(weights);
-%$ expectation = sum(weights.*nodes);
-%$ variance = sum(weights.*(nodes.^2));
-%$
-%$ % Check the results.
-%$ t(1) = dassert(1.0,sum_of_weights,1e-12);
-%$ t(2) = dassert(1.0,variance,1e-12);
-%$ t(3) = dassert(0.0,expectation,1e-12);
-%$ T = all(t);
+n = 9;
+[nodes,weights] = gauss_hermite_weights_and_nodes(n);
+
+sum_of_weights = sum(weights);
+expectation = sum(weights.*nodes);
+variance = sum(weights.*(nodes.^2));
+
+% Check the results.
+t(1) = dassert(1.0,sum_of_weights,1e-12);
+t(2) = dassert(1.0,variance,1e-12);
+t(3) = dassert(0.0,expectation,1e-12);
+T = all(t);
 %@eof:2
 
 %@test:3
-%$ n = 9;
-%$ [nodes,weights] = gauss_hermite_weights_and_nodes(n);
-%$
-%$ NODES = cartesian_product_of_sets(nodes,nodes);
-%$ WEIGHTS = cartesian_product_of_sets(weights,weights);
-%$ WEIGHTS = prod(WEIGHTS,2);
-%$
-%$ sum_of_weights = sum(WEIGHTS);
-%$ expectation = transpose(WEIGHTS)*NODES;
-%$ variance = transpose(WEIGHTS)*NODES.^2;
-%$
-%$ % Check the results.
-%$ t(1) = dassert(1.0,sum_of_weights,1e-12);
-%$ t(2) = dassert(ones(1,2),variance,1e-12);
-%$ t(3) = dassert(zeros(1,2),expectation,1e-12);
-%$ T = all(t);
+n = 9;
+[nodes,weights] = gauss_hermite_weights_and_nodes(n);
+
+NODES = cartesian_product_of_sets(nodes,nodes);
+WEIGHTS = cartesian_product_of_sets(weights,weights);
+WEIGHTS = prod(WEIGHTS,2);
+
+sum_of_weights = sum(WEIGHTS);
+expectation = transpose(WEIGHTS)*NODES;
+variance = transpose(WEIGHTS)*NODES.^2;
+
+% Check the results.
+t(1) = dassert(1.0,sum_of_weights,1e-12);
+t(2) = dassert(ones(1,2),variance,1e-12);
+t(3) = dassert(zeros(1,2),expectation,1e-12);
+T = all(t);
 %@eof:3
 
 %@test:4
-%$ n = 9; sigma = .1;
-%$ [nodes,weights] = gauss_hermite_weights_and_nodes(n);
-%$
-%$ sum_of_weights = sum(weights);
-%$ expectation = sum(weights.*nodes*.1);
-%$ variance = sum(weights.*((nodes*.1).^2));
-%$
-%$ % Check the results.
-%$ t(1) = dassert(1.0,sum_of_weights,1e-12);
-%$ t(2) = dassert(.01,variance,1e-12);
-%$ t(3) = dassert(0.0,expectation,1e-12);
-%$ T = all(t);
+n = 9; sigma = .1;
+[nodes,weights] = gauss_hermite_weights_and_nodes(n);
+
+sum_of_weights = sum(weights);
+expectation = sum(weights.*nodes*.1);
+variance = sum(weights.*((nodes*.1).^2));
+
+% Check the results.
+t(1) = dassert(1.0,sum_of_weights,1e-12);
+t(2) = dassert(.01,variance,1e-12);
+t(3) = dassert(0.0,expectation,1e-12);
+T = all(t);
 %@eof:4
