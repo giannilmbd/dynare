b2e70b02379212d6258aced25d2830832ff42053
Johannes Pfeifer <jpfeifer@gmx.de>
2026-01-23 12:31:00 +0100

    Saving and setting RNG states: also pass affected stream
    Required in non_linear_dsge_likelihood.m where parfor may alter the default stream in between

--- matlab/set_dynare_random_generator_state.m
+++ matlab/set_dynare_random_generator_state.m
@@ -1,12 +1,20 @@
-function [state_u,state_n] = set_dynare_random_generator_state(state_u,state_n)
-% Wet state of MATLAB/Octave random generator depending on MATLAB
-% (Octave) version.
-% In older versions, MATLAB kept one generator for uniformly distributed numbers and
-% one for normally distributed numbers.
-% For backward compatibility, we return two vectors, but, in recent
-% versions of MATLAB and in Octave, we return two identical vectors.
+function set_dynare_random_generator_state(state_u,state_n,current_stream)
+%Set state of MATLAB/Octave random generator 
+% Outputs:
+%   - None
+% Inputs:
+%   - state_u           [integer]       state of uniform RNG
+%   - state_n           [integer]       state of normal RNG
+%   - global_stream     [randstream]    RNG stream to be made default stream
 %
-% Copyright © 2010-2020 Dynare Team
+% Notes: 
+%  - Octave, like older versions of MATLAB keeps one generator for uniformly distributed numbers and
+%    one for normally distributed numbers. 
+%  - For compatibility, we input two vectors, which need to be identical for MATLAB. 
+%  - For MATLAB, we also meed to define the current stream as it may have changed when
+%       parfor was used
+
+% Copyright © 2010-2026 Dynare Team
 %
 % This file is part of Dynare.
 %
@@ -24,25 +32,22 @@ function [state_u,state_n] = set_dynare_random_generator_state(state_u,state_n)
 % along with Dynare.  If not, see <https://www.gnu.org/licenses/>.
 
 if ~isoctave
-    s = RandStream.getGlobalStream();
-    if isequal(s.Type,'legacy')
-        rand('state',state_u);
-        randn('state',state_n);
-    else
-        if ~isequal(state_u,state_n)
-            error(['You are using the new MATLAB RandStream mechanism ' ...
-                   'with a single random generator, but the values ' ...
-                   'of the state of the uniformly ' ...
-                   'distributed numbers and of the state of the ' ...
-                   'normally distributed numbers are different. Something must be ' ...
-                   'wrong, such as reloading old Metropolis runs, ' ...
-                   'computed on a different version of MATLAB. If you ' ...
-                   'don''t understand the origin of the problem, ' ...
-                   'please, contact Dynare''s development team.'])
-        end
-        s.State = state_u;
-        RandStream.setGlobalStream(s);
+    if nargin<3
+        current_stream = RandStream.getGlobalStream();
     end
+    if ~isequal(state_u,state_n)
+        error(['You are using the new MATLAB RandStream mechanism ' ...
+            'with a single random generator, but the values ' ...
+            'of the state of the uniformly ' ...
+            'distributed numbers and of the state of the ' ...
+            'normally distributed numbers are different. Something must be ' ...
+            'wrong, such as reloading old Metropolis runs, ' ...
+            'computed on a different version of MATLAB. If you ' ...
+            'don''t understand the origin of the problem, ' ...
+            'please, contact Dynare''s development team.'])
+    end
+    current_stream.State = state_u;
+    RandStream.setGlobalStream(current_stream);
 else
     rand('state',state_u);
     randn('state',state_n);
