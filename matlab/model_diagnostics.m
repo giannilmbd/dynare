c99b495b0230a55a343c562a94e5939f41362aaf
Johannes Pfeifer <jpfeifer@gmx.de>
2026-01-30 11:22:50 +0100

    model_diagnostics.m: add singularity check for contemporaneous dynamic Jacobian

--- matlab/model_diagnostics.m
+++ matlab/model_diagnostics.m
@@ -393,6 +393,86 @@ if rank_jacob_dyn < M_.endo_nbr
     end
 end
 
+%
+% singular contemporaneous Jacobian of dynamic model
+%
+jacob_contemp = jacob_dyn(:, M_.endo_nbr+1:2*M_.endo_nbr);
+
+try
+    if (~isoctave && matlab_ver_less_than('9.12')) || isempty(options_.jacobian_tolerance)
+        rank_jacob_contemp = rank(jacob_contemp);
+    else
+        rank_jacob_contemp = rank(jacob_contemp, options_.jacobian_tolerance);
+    end
+catch
+    rank_jacob_contemp = size(jacob_contemp, 1);
+end
+
+if rank_jacob_contemp < M_.endo_nbr
+    problem_dummy = 1;
+    skipline(1);
+    disp(['MODEL_DIAGNOSTICS:  The contemporaneous part of the Jacobian of the dynamic model is ' ...
+          'singular'])
+    disp(['MODEL_DIAGNOSTICS:  there is ' num2str(M_.endo_nbr - rank_jacob_contemp) ...
+          ' collinear relationships between the variables and the equations'])
+    ncol = compute_nullspace(jacob_contemp, options_.jacobian_tolerance);
+    n_rel = size(ncol, 2);
+    for i = 1:n_rel
+        if n_rel > 1
+            disp(['Relation ' int2str(i)])
+        end
+        disp('Collinear variables:')
+        for j = 1:10
+            k = find(abs(ncol(:, i)) > 10^-j);
+            if max(abs(jacob_contemp(:, k) * ncol(k, i))) < 1e-6
+                break
+            end
+        end
+        var_names = endo_names(k);
+        if length(var_names) > 5
+            % Print 10 variables per row
+            for row_start = 1:10:length(var_names)
+                row_end = min(row_start + 9, length(var_names));
+                fprintf('    %s\n', strjoin(var_names(row_start:row_end), ', '));
+            end
+        else
+            for v_iter = 1:length(var_names)
+                fprintf('    %s\n', var_names{v_iter});
+            end
+        end
+    end
+    neq = compute_nullspace(jacob_contemp', options_.jacobian_tolerance);
+    n_rel = size(neq, 2);
+    for i = 1:n_rel
+        if n_rel > 1
+            disp(['Relation ' int2str(i)])
+        end
+        disp('Collinear equations:')
+        for j = 1:10
+            k = find(abs(neq(:, i)) > 10^-j);
+            if max(abs(jacob_contemp(k, :)' * neq(k, i))) < 1e-6
+                break
+            end
+        end
+        for eq_iter = 1:length(k)
+            eq_nbr = k(eq_iter);
+            % Find the 'name' tag for this equation in M_.equations_tags
+            eq_name = '';
+            if isfield(M_, 'equations_tags') && ~isempty(M_.equations_tags)
+                name_idx = find([M_.equations_tags{:,1}]' == eq_nbr & strcmp(M_.equations_tags(:,2), 'name'));
+                if ~isempty(name_idx)
+                    eq_name = M_.equations_tags{name_idx(1), 3};
+                end
+            end
+            if ~isempty(eq_name)
+                fprintf('    Equation %d: %s\n', eq_nbr, eq_name);
+            else
+                fprintf('    Equation %d\n', eq_nbr);
+            end
+        end
+    end
+end
+
 if exist('g2_v','var')
     if any(any(isinf(g2_v) | isnan(g2_v)))
         problem_dummy=1;
