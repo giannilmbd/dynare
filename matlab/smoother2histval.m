giannilmbd/dynare

commit 2dd6510051304f2c534ad4d10bdd0b7d88ebdfaf

smoother2histval.m: undo logging
Closes https://git.dynare.org/Dynare/dynare/-/issues/1407

matlab/smoother2histval.m
@@ -179,7 +179,11 @@ end
 % Handle all endogenous variables to be copied
 for i = 1:length(invars)
     if ~isempty(strmatch(invars{i}, M_.endo_names, 'exact')) 
-        s = smoothedvars.(invars{i});
+        if oo_.Smoother.loglinear
+            s = exp(smoothedvars.(invars{i}));
+        else
+            s = smoothedvars.(invars{i});
+        end
     elseif ~isempty(strmatch(invars{i}, M_.exo_names, 'exact'))
         s = smoothedshocks.(invars{i});
     else

tests/smoother2histval/fs2000_smooth_stoch_simul.mod
@@ -82,10 +82,11 @@ varobs gp_obs gy_obs;
 
 options_.solve_tolf = 1e-12;
 
-estimation(order=1,datafile=fsdat_simul,nobs=192,mh_replic=1500,mh_nblocks=1,mh_jscale=0.8,smoother,consider_all_endogenous);
-
+estimation(order=1,loglinear,datafile=fsdat_simul,nobs=192,mh_replic=2,mh_nblocks=1,mh_jscale=0.8,smoother,consider_all_endogenous_and_auxiliary);
+steady;
 smoother2histval(period = 5);
 
+options_.loglinear=0;
 stoch_simul(nomoments);
 
 forecast;

commit f1c3e17bcd25902233cea8a001832921b42f80f2

Drop the “infile” option of the “smoother2histval” command
It is redundant with “outfile” option + “histval_file” command.

Moreover, the option was broken since commit
2dd6510051304f2c534ad4d10bdd0b7d88ebdfaf, and nobody noticed since.

matlab/smoother2histval.m
@@ -1,11 +1,9 @@
 function smoother2histval(opts)
 % This function takes values from oo_.SmoothedVariables (and possibly
-% oo_.SmoothedShocks) and copies them into M_.histval.
+% oo_.SmoothedShocks) and copies them into M_.endo_histval or in a file to be
+% read by the “histval_file” command.
 %
 % Optional fields in 'opts' structure:
-%    infile:      An optional *_results MAT file created by Dynare.
-%                 If present, oo_.Smoothed{Variables,Shocks} are read from
-%                 there. Otherwise, they are read from the global workspace.
 %    invars:      An optional char or cell array listing variables to read in
 %                 oo_.SmoothedVariables. If absent, all the endogenous
 %                 variables present in oo_.SmoothedVariables are used.
@@ -41,20 +39,11 @@ function smoother2histval(opts)
 
 global M_ options_ oo_
 
-if ~isfield(opts, 'infile')
-    if ~isfield(oo_, 'SmoothedVariables')
-        error('Could not find smoothed variables; did you set the "smoother" option?')
-    end
-    smoothedvars = oo_.SmoothedVariables;
-    smoothedshocks = oo_.SmoothedShocks;
-else
-    S = load(opts.infile);
-    if ~isfield(S, 'oo_') || ~isfield(S.oo_, 'SmoothedVariables')
-        error('Could not find smoothed variables in file; is this a Dynare results file, and did you set the "smoother" option when producing it?')
-    end
-    smoothedvars = S.oo_.SmoothedVariables;
-    smoothedshocks = S.oo_.SmoothedShocks;
+if ~isfield(oo_, 'SmoothedVariables')
+    error('Could not find smoothed variables, did you compute them? (either by the "smoother" option of the "estimation" command, or the "calib_smoother" command')
 end
+smoothedvars = oo_.SmoothedVariables;
+smoothedshocks = oo_.SmoothedShocks;
 
 % Hack to determine if oo_.SmoothedVariables was computed after a Metropolis
 tmp = fieldnames(smoothedvars);