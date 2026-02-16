giannilmbd/dynare

commit b3b9b1e5d6414d6163d0863e2478b2b973567788

set_dynare_seed_local_options.m: make sure algo field is set for Octave
Required for empty initialization introduced in 24767e9d

matlab/set_dynare_seed_local_options.m
@@ -2,17 +2,17 @@ function DynareRandomStreams=set_dynare_seed_local_options(DynareRandomStreams,i
 % DynareRandomStreams=set_dynare_seed_local_options(DynareRandomStreams,isHybridMatlabOctave,a,b)
 % Set seeds depending on MATLAB (octave) version
 % Inputs:
-%   o DynareRandomStreams   options structure relating to options_.DynareRandomStreams, 
+%   o DynareRandomStreams   options structure relating to options_.DynareRandomStreams,
 %                           potentially empty for initialization (a=='default')
 %   o isHybridMatlabOctave  bool indicating whether parallel mixed pool is
 %                           used
-%   o a                     first input argument, 
+%   o a                     first input argument,
 %                           for single argument input, either
 %                               [number]       seed
 %                               [string]        'default'
 %                                               'reset'
-%       	                                    'clock'
-%                           for two inputs: 
+%       	                                      'clock'
+%                           for two inputs:
 %                               [string]        indicating feasible RNG algorithm (default: 'mt19937ar')
 %   o b                     second input argument
 %                               [number]        seed for algorithm
@@ -104,6 +104,14 @@ else% Use old MATLAB interface.
             rand(DynareRandomStreams.algo,DynareRandomStreams.seed);
             randn('state',DynareRandomStreams.seed);
             return
+        end
+        if isempty(DynareRandomStreams) || isfield(DynareRandomStreams,'algo')
+        %make sure algorithm is set
+            if isoctave
+                DynareRandomStreams.algo = 'state';
+            else
+                DynareRandomStreams.algo = 'twister';
+            end
         end
         if ischar(a) && strcmpi(a,'reset')
             rand(DynareRandomStreams.algo,DynareRandomStreams.seed);

commit 180d3b480a25446b1755df4f54192793ee6a9d16

set_dynare_seed_local_options.m: fix Octave seed logic bug introduced in b3b9b1e5

matlab/set_dynare_seed_local_options.m
@@ -106,7 +106,7 @@ else% Use old MATLAB interface.
             randn('state',DynareRandomStreams.seed);
             return
         end
-        if isempty(DynareRandomStreams) || isfield(DynareRandomStreams,'algo')
+        if isempty(DynareRandomStreams) || ~isfield(DynareRandomStreams,'algo')
         %make sure algorithm is set
             if isoctave
                 DynareRandomStreams.algo = 'state';