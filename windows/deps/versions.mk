90f0c10e81095337abaf77b9c515556c6f81f3eb
Sébastien Villemot <sebastien@dynare.org>
2026-02-19 14:00:03 +0100

    📦 Windows package: bump dependencies

--- windows/deps/versions.mk
+++ windows/deps/versions.mk
@@ -21,28 +21,28 @@ SUITESPARSE_VERSION = 7.12.2
 
 # pacman -Ss mingw-w64-ucrt-x86_64-boost
 # (also used by the preprocessor CI, so it can be updated there also)
-MSYS2_BOOST_VERSION = 1.90.0-2
+MSYS2_BOOST_VERSION = 1.90.0-3
 
 ## Packages for the embedded compiler
 
 # pacman -Ss mingw-w64-ucrt-x86_64-gcc$
-MSYS2_GCC_VERSION = 15.2.0-9
+MSYS2_GCC_VERSION = 15.2.0-11
 
 # Dependency of gcc, isl, mpc and mpfr
 # pacman -Ss mingw-w64-ucrt-x86_64-gmp
 MSYS2_GMP_VERSION = 6.3.0-2
 
 # pacman -Ss mingw-w64-ucrt-x86_64-binutils
-MSYS2_BINUTILS_VERSION = 2.45.1-1
+MSYS2_BINUTILS_VERSION = 2.46-2
 
 # pacman -Ss mingw-w64-ucrt-x86_64-headers
-MSYS2_HEADERS_VERSION = 13.0.0.r391.g848cce552-1
+MSYS2_HEADERS_VERSION = 13.0.0.r505.g7d006b2ea-1
 
 # pacman -Ss mingw-w64-ucrt-x86_64-crt
-MSYS2_CRT_VERSION = 13.0.0.r391.g848cce552-1
+MSYS2_CRT_VERSION = 13.0.0.r505.g7d006b2ea-1
 
 # pacman -Ss mingw-w64-ucrt-x86_64-winpthreads
-MSYS2_WINPTHREADS_VERSION = 13.0.0.r391.g848cce552-1
+MSYS2_WINPTHREADS_VERSION = 13.0.0.r505.g7d006b2ea-1
 
 # pacman -Ss mingw-w64-ucrt-x86_64-isl
 MSYS2_ISL_VERSION = 0.27-1
@@ -65,7 +65,7 @@ MSYS2_ZSTD_VERSION = 1.5.7-1
 
 # Dependency of binutils
 # pacman -Ss mingw-w64-ucrt-x86_64-gettext-runtime
-MSYS2_GETTEXT_RUNTIME_VERSION = 0.26-2
+MSYS2_GETTEXT_RUNTIME_VERSION = 1.0-1
 
 # Dependency of gettext-runtime
 # pacman -Ss mingw-w64-ucrt-x86_64-libiconv
